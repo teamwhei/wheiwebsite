b08f12e5fb66e6d8de4e3a7f905741b4a3c11a55
hansheng quek <hsquek06@gmail.com>
2017-08-18 14:25:26 +0800

    added portfolio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal()
+  $(document).on('click', '.whei', function (e) {
+    $(this).hide()
+    $('.portfolio').show(2000)
+  })
 })
