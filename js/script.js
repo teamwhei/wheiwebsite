a64e019b2cf3e6bafde23c4cfb6604af292f3b0c
hansheng quek <hsquek06@gmail.com>
2017-08-18 14:45:55 +0800

    can now toggle between portfolio and logo

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 
   $('.modal').modal()
-  
+
   $(document).on('click', '.whei', function (e) {
     $(this).hide()
     $('.portfolio').show(2000)
+  })
+
+  $(document).on('click', '.portfolio', function (e) {
+    $(this).hide()
+    $('.whei').show(2000)
   })
 })
